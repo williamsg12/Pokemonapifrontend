3b1cec5c8d44875405573af3c13c9496e29126cf
Gary <williamsg12>
2021-10-21 15:04:35 -0400

    worked

--- pokemon-frontend/src/Components/Pokemon/Pokemon.js
+++ pokemon-frontend/src/Components/Pokemon/Pokemon.js
@@ -1,6 +1,12 @@
+import axios from 'axios';
 import React from 'react';
-
+import { useEffect } from 'react'
 const Pokemon = () => {
+
+    useEffect(() => {
+        axios.get('https://localhost:8000')
+    }, [])
+
     return (
         <div>
             
--- pokemon-frontend/src/Components/Trainers/Trainer.js
+++ pokemon-frontend/src/Components/Trainers/Trainer.js
@@ -1,6 +1,10 @@
 import React from 'react';
-
+import { useEffect } from 'react';
+import axios
 const Trainer = () => {
+        useEffect(() => {
+					axios.get('https://localhost:8000');
+				}, []);
     return (
         <div>
             
